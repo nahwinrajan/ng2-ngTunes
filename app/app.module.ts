2bf9939e6473a8ca1074a739f7eb8673fae3532c
nahwinrajan <nahwin.rajan@gmail.com>
2016-10-31 01:29:52 +0700

    done scaffold and router setup

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,16 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }           from '@angular/core';
+import { BrowserModule }      from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import { AppComponent }       from './app.component';
+import { NavbarComponent }    from './components/navbar/navbar.component';
+import { AboutComponent }     from './components/about/about.component';
+import { SearchComponent }    from './components/search/search.component';
+
+import { Routing }      from './app.routing';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, Routing ],
+  declarations: [ AppComponent, NavbarComponent, AboutComponent, SearchComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
